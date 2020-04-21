ec8917b51b0526b816451a5712900ba09370b7a2
Joshua Medlen <joshua246@gmail.com>
2020-04-20 19:07:10 -0500

    added first function

---
+++ functions.js
@@ -0,0 +1,42 @@
+// const chicken = 'chicken'
+// console.log(`process ${chicken}`)
+
+// const chicken = 'cow'
+// console.log(`process ${cow}`)
+
+// const chicken = 'pig'
+// console.log(`process ${pig}`)
+
+// const tuna = 'tuna'
+// console.log(`process ${tuna}`)
+
+// const nuggatizer = (animal) => {
+//   return `process ${animal}`
+// }
+
+// console.log(nuggatizer('chicken'))
+// console.log(nuggatizer('cow'))
+// console.log(nuggatizer('pig'))
+// console.log(nuggatizer('tuna'))
+
+// const dogBreed = (breed) => {
+//   return `My favorite dog breed is ${breed.toUpperCase()}`;
+// }
+
+// console.log(dogBreed('lab'))
+// console.log(dogBreed('german sheppard'))
+
+const numPlus = (num) => {
+  return num + 42;
+}
+
+console.log(numPlus(5))
+
+
+// write a function that takes the year you were born and figures out how old you will be in 2099
+
+const ageOld = (num) => {
+  return 2099 - num;
+}
+
+console.log(ageOld(1990))
